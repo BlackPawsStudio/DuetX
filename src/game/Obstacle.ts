077c236013d2b1d08d4f700f332f476534669d4c
Artsemi Bal <a.bal@be-infinity.com>
2023-10-08 21:54:54 +0300

    add collision for square

--- src/game/Obstacle.ts
+++ src/game/Obstacle.ts
@@ -34,7 +34,7 @@ export class Obstacle {
           this.ctx.fillRect(
             canvas.width / 2,
             this.y - offset,
-            size / 2.5,
+            size,
             size / 2.5
           );
         } else {
@@ -44,9 +44,9 @@ export class Obstacle {
       case "right":
         if (this.type === "square") {
           this.ctx.fillRect(
-            canvas.width / 2 - size / 2.5,
+            canvas.width / 2 - size,
             this.y - offset,
-            size / 2.5,
+            size,
             size / 2.5
           );
         } else {
@@ -60,6 +60,8 @@ export class Obstacle {
         break;
     }
     this.y += speed;
+
+    //  reached end
     if (this.y - offset - size / 7 > canvas.height) {
       this.y = offset;
       this.side = Math.random() + this.percent > 1 ? "left" : "right";
@@ -74,6 +76,11 @@ export class Obstacle {
 
   checkCollision(radius: number, offset: number, rotationAngle: number) {
     const canvas = this.ctx.canvas;
+
+    const size = Math.round(
+      canvas.width > canvas.height ? canvas.width / 10 : canvas.height / 5
+    );
+
     const heightL = canvas.height / 2 - radius * 7 * Math.sin(rotationAngle);
 
     const heightR = canvas.height / 2 + radius * 7 * Math.sin(rotationAngle);
@@ -83,19 +90,39 @@ export class Obstacle {
       this.side === "left"
         ? angle > 90 && angle < 270
         : angle < 90 || angle > 270;
-    if (
-      safeZone &&
-      heightL - radius < this.y - offset &&
-      heightL + radius > this.y - offset
-    ) {
-      return "#23acff";
-    }
-    if (
-      !safeZone &&
-      heightR - radius < this.y - offset &&
-      heightR + radius > this.y - offset
-    ) {
-      return "red";
+
+    //  simple
+    if (this.type === "simple") {
+      if (
+        safeZone &&
+        heightL - radius < this.y - offset &&
+        heightL + radius > this.y - offset
+      ) {
+        return "#23acff";
+      }
+      if (
+        !safeZone &&
+        heightR - radius < this.y - offset &&
+        heightR + radius > this.y - offset
+      ) {
+        return "red";
+      }
+      //  square
+    } else if (this.type === "square") {
+      if (
+        safeZone &&
+        heightL < this.y - offset + size / 2.5 &&
+        heightL > this.y - offset - 10
+      ) {
+        return "#23acff";
+      }
+      if (
+        !safeZone &&
+        heightR < this.y - offset + size / 2.5 &&
+        heightR > this.y - offset - 10
+      ) {
+        return "red";
+      }
     }
     return false;
   }
